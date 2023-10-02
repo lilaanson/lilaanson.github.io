e872a96d2ba0e867e9424a6d66e415d4df000cc8
lilaanson <123125018+lilaanson@users.noreply.github.com>
2023-10-01 18:23:52 -0600

    UGH

--- extra/quan/vector_add_sum_part2.py
+++ extra/quan/vector_add_sum_part2.py
@@ -1,7 +1,29 @@
 import unittest
+import math
 
 def vector_add(a,b):
     c = []
     for i in range(len(a)):
         c.append(a[i]+b[i])
     return(c)
+
+def vector_sum(a):
+    total = 0
+    for i in a:
+        total += i
+    return(total)
+
+def norm(a):
+    step_one = 0
+    for i in a:
+        step_one += i * i
+    e_norm = math.sqrt(step_one)
+    return(e_norm)
+
+
+
+class TestFunction(unittest.TestCase):
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
